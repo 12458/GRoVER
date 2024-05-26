8178fc192509f1efae03821551303152dd90d126
Sim Shang En <6071014+12458@users.noreply.github.com>
2024-05-26 13:01:10 +0800

    Add paper download functionalilty based on google scholar

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-
-from scidownl import scihub_download
-from scholarly import scholarly
+from core.scraper.paper_manager import PaperManager
 
 
 app = FastAPI()
@@ -13,14 +11,28 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/paper")
+@app.get("/paper/download")
 def read_item(doi: str):
-    scihub_download(doi, paper_type="doi", out=f"/tmp/{doi}.pdf")
+    pm = PaperManager()
+    path = pm.download_paper(doi)
     # serve the file
-    return FileResponse(path=f"/tmp/{doi}.pdf")
+    return FileResponse(path=path)
+
 
 @app.get("/paper/search")
 def search_paper(q: str):
-    scihub_download(q, paper_type="search", out=f"/tmp/{query}.pdf")
-    # serve the file
-    return FileResponse(path=f"/tmp/{query}.pdf"
+    pm = PaperManager()
+    result = pm.search_paper(q)
+    return {"result": result}
+
+
+@app.get("/paper/search_query_download")
+def search_query_download(q: str):
+    pm = PaperManager()
+    result = pm.search_paper(q)
+    bibtex = pm.get_bibtex(result[0])
+    doi = pm.get_doi(bibtex)
+    if doi:
+        path = pm.download_paper(doi)
+        return FileResponse(path=path)
+    return {"error": "No paper found"}
